aaadceff75618774b94cd09c497a551d0de60640
patelaftab <10patelaftab@gmail.com>
2023-02-08 20:55:29 +0530

    [Aftab] : Checking Player Start Position

---
+++ SnakeAndLadderProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SnakeLadder snakeLadder=new SnakeLadder();
+            snakeLadder.Game();
+        }
+    }
+}
---
+++ SnakeAndLadderProblem/SnakeLadder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    public class SnakeLadder
+    {
+        public void Game()
+        {
+             int playerposition = 0;
+            Console.WriteLine("Player Starting Position :" +playerposition);
+            Console.ReadLine();
+        }
+    }
+}
